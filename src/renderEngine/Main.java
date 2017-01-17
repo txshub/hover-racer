434995ab5a0d998de9403d8a178ddfd080fe5247
Reece Bennett <reece.bennett1@gmail.com>
2017-01-17 17:59:44 +0000

    Make basic render class

--- src/renderEngine/Main.java
+++ src/renderEngine/Main.java
@@ -1,81 +1,54 @@
 package renderEngine;
 
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-import java.nio.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.opengl.GL11.*;
+
+import org.lwjgl.opengl.GL;
 
 public class Main {
-  private long window;
   
-   public void run() {
-     init();
-     loop();
-     
-     // Free the window callbacks and destroy the window
-     glfwFreeCallbacks(window);
-     glfwDestroyWindow(window);
+  long window;
 
-     // Terminate GLFW and free the error callback
-     glfwTerminate();
-     glfwSetErrorCallback(null).free();
-   }
-   
-   private void init() {
-     // Set the error callback
-     GLFWErrorCallback.createPrint(System.err).set();
-     
-     // Initialise GLFW
-     if (!glfwInit()) {
-       throw new IllegalStateException("Unable to initialise GLFW");
-     }
-     
-     // Create the game window
-     window = glfwCreateWindow(640, 480, "Hover-Racer", NULL, NULL);
-     if (window == NULL) {
-       glfwTerminate();
-       throw new RuntimeException("Failed to create the GLFW window");
-     }
-     
-     // Setup a key callback
-     glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-       if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-         glfwSetWindowShouldClose(window, true);
-     });
-     
-     // Create the OpenGL context for rendering
-     glfwMakeContextCurrent(window);
-     // Enable v-sync
-     glfwSwapInterval(1);
-   }
-   
-   private void loop() {
-     GL.createCapabilities();
-     
-     glClearColor(0.57f, 0.32f, 0.75f, 0.0f);
-     
-     // Run the rendering loop until the user has attempted to close the 
-     // window or has pressed the ESCAPE key.
-     while ( !glfwWindowShouldClose(window) ) {
-       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+  public Main() {
+    if (!glfwInit()) {
+      throw new IllegalStateException("Unable to initialize GLFW");
+    }
+    
+    window = glfwCreateWindow(640, 480, "Hover-Racer", NULL, NULL);
+    
+    glfwShowWindow(window);
+    
+    glfwMakeContextCurrent(window);
+    GL.createCapabilities();
+    
+    while(!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+      
+      glClear(GL_COLOR_BUFFER_BIT);
+      
+      glBegin(GL_QUADS);
+        glColor4f(1, 0, 0, 0);
+        glVertex2f(-0.5f, 0.5f);
+        
+        glColor4f(0, 1, 0, 0);
+        glVertex2f(0.5f, 0.5f);
 
-       glfwSwapBuffers(window); // swap the color buffers
+        glColor4f(0, 0, 1, 0);
+        glVertex2f(0.5f, -0.5f);
 
-       // Poll for window events. The key callback above will only be
-       // invoked during this call.
-       glfwPollEvents();
-     }
-   }
-   
-   public static void main(String[] args) {
-     new Main().run();
-   }
+        glColor4f(1, 1, 1, 0);
+        glVertex2f(-0.5f, -0.5f);
+      glEnd();
+      
+      glfwSwapBuffers(window);
+    }
+    
+    glfwTerminate();
+  }
+
+  public static void main(String[] args) {
+    new Main();
+  }
+
 }
