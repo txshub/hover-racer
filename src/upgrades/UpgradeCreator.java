8ad1ef57389adc353c216f2be5d187e762d28e29
mxb551 <mxb551@cs.bham.ac.uk>
2017-02-07 14:49:07 +0000

    Made UpgradeCreator save JSON to files

--- src/upgrades/UpgradeCreator.java
+++ src/upgrades/UpgradeCreator.java
@@ -1,5 +1,7 @@
-package upgrades.data;
+package upgrades;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,9 +9,6 @@ import java.util.Optional;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
-
-import upgrades.Stat;
-import upgrades.UpgradeType;
 
 public class UpgradeCreator {
 
@@ -22,10 +21,10 @@ public class UpgradeCreator {
 			System.out.println("Pick a stat or hit enter to finish. Available stats:");
 			Arrays.stream(Stat.values()).forEach(s -> System.out.print(s.getName() + ", "));
 			System.out.println();
-			String inStat = in.nextLine();
+			String inStat = in.nextLine().toLowerCase();
 			if (inStat.equals("")) break; // If empty line then finish adding stats
 			System.out.println(inStat);
-			Optional<Stat> stat = Arrays.stream(Stat.values()).filter(s -> s.getName().equals(inStat)).findAny();
+			Optional<Stat> stat = Arrays.stream(Stat.values()).filter(s -> s.getName().toLowerCase().equals(inStat)).findAny();
 			if (stat.isPresent()) {
 				System.out.println("Pick a value");
 				float value = Float.parseFloat(in.nextLine());
@@ -39,10 +38,22 @@ public class UpgradeCreator {
 		int maxLevel = Integer.parseInt(in.nextLine());
 		if (maxLevel < -1) maxLevel = -1;
 		UpgradeType upgrade = new UpgradeType(name, stats, maxLevel, cost);
-		System.out.println((new Gson()).toJson(upgrade));
+		String json = (new Gson()).toJson(upgrade);
+		System.out.println("Created " + json);
+		save(name, json);
 		System.out.println("Keep going?");
 		String ans = in.nextLine();
 		if (ans.equals("") || ans.equals("y") || ans.equals("yes") || ans.equals("ok") || ans.equals("go")) main(null);
+	}
+
+	public static void save(String name, String content) {
+		try {
+			PrintWriter writer = new PrintWriter("src/upgrades/data/" + name + ".txt", "UTF-8");
+			writer.println(content);
+			writer.close();
+		} catch (IOException e) {
+			System.err.println("Failed to write to file");
+		}
 	}
 
 
