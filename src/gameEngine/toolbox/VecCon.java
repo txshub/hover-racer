110dd9674008c879fb25357c6529dd3f33543c7f
Reece Bennett <reece.bennett1@gmail.com>
2017-02-11 14:06:09 +0000

    Fix VecCon returning wrong vector types

--- src/gameEngine/toolbox/VecCon.java
+++ src/gameEngine/toolbox/VecCon.java
@@ -4,12 +4,12 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class VecCon {
 
-	public static Vector3f toJOML(org.joml.Vector3f vec){
+	public static Vector3f toLWJGL(org.joml.Vector3f vec){
 		return new Vector3f(vec.x,vec.y,vec.z);
 		
 	}
 	
-	public static org.joml.Vector3f toLWJGL(Vector3f vec){
+	public static org.joml.Vector3f toJOML(Vector3f vec){
 		return new org.joml.Vector3f(vec.x,vec.y,vec.z);
 		
 	}
