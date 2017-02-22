5cd595586071289032b481221086c73e3ba46c0a
Reece <Reece@DESKTOP-VCTRSCT>
2017-02-22 18:05:38 +0000

    updates to classConvertor

--- src/clientComms/ClassConvertor.java
+++ src/clientComms/ClassConvertor.java
@@ -4,8 +4,9 @@ import java.nio.ByteBuffer;
 import java.util.HashMap;
 
 /**
- * @author rtm592 converts classes into compact bytes messages and back
- * this method allows for 256 different message types to be sent along the network
+ * @author rtm592 converts classes into compact bytes messages and back this
+ *         method allows for 256 different message types to be sent along the
+ *         network
  */
 public class ClassConvertor {
 
@@ -22,8 +23,7 @@ public class ClassConvertor {
 	}
 
 	/**
-	 * converts a byte[] to a float 
-	 * messageid = 0
+	 * converts a byte[] to a float messageid = 0
 	 * 
 	 * @param bytes
 	 *            the sent message
@@ -34,8 +34,7 @@ public class ClassConvertor {
 	}
 
 	/**
-	 * converts sent byte message into a hashmap leaderboard
-	 * messageID = 1
+	 * converts sent byte message into a hashmap leaderboard messageID = 1
 	 * 
 	 * @param bytes
 	 *            the sent message in the form of ship ID, place, ship ID, place
@@ -44,7 +43,7 @@ public class ClassConvertor {
 	 */
 	public static HashMap<Integer, Integer> getRankings(byte[] bytes) {
 
-		if (bytes.length % 2 == 1) {
+		if (bytes.length - 1 % 2 == 1 || bytes[0] != (byte) 1) {
 			// error should not occur
 			System.err.println("error something is wrong");
 			return new HashMap<>();
@@ -52,33 +51,56 @@ public class ClassConvertor {
 		HashMap<Integer, Integer> leaderboard = new HashMap<>();
 
 		for (int i = 0; i < bytes.length / 2; i++) {
-			int shipid = (int) bytes[i * 2];
-			int place = (int) bytes[i * 2];
+			int shipid = (int) bytes[i * 2 + 1];
+			int place = (int) bytes[i * 2 + 2];
 			leaderboard.put(place, shipid);
 		}
 
-		return new HashMap<>();
+		return leaderboard;
 
 	}
 
 	/**
-	 * converts the leaderboard into a byte message
-	 * messageID = 1
+	 * converts the leaderboard into a byte message messageID = 1
 	 * 
-	 * @param leaderboard the leaderboard
+	 * @param leaderboard
+	 *            the leaderboard
 	 * @return the message
 	 */
 	public static byte[] getRankingsMessage(HashMap<Integer, Integer> leaderboard) {
 
+		// under the assumption that ships have incrementing ids in the game
+		// starting at 0
 		byte[] message = new byte[leaderboard.size() * 2 + 1];
-		message[1] = (byte) 1;
+		message[0] = (byte) 1;
 
 		for (Integer place : leaderboard.keySet()) {
-
+			message[place * 2 + 1] = (byte) place.intValue();
+			message[place * 2 + 2] = (byte) leaderboard.get(place).intValue();
 		}
 
 		return message;
 
+	}
+/**
+ * for testing the class
+ * need J-units
+ */
+	public static void main(String[] args) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		map.put(0, 7);
+		map.put(1, 30);
+		map.put(2, 56);
+		map.put(3, 87);
+
+		map = getRankings(getRankingsMessage(map));
+
+		for (Integer place : map.keySet()) {
+			System.out.println(place + ", " + map.get(place));
+		}
+
+		float out = getDistancefloat(getDistanceMessage(10f));
+		System.out.println("should be 10.0: "+out);
 	}
 
 }
