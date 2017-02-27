fc6560cc0a98054c8a322bfdc049355e51443a75
Reece Bennett <reece.bennett1@gmail.com>
2017-02-27 21:41:57 +0000

    Create UIElement superclass of container and button

--- src/uiToolkit/Button.java
+++ src/uiToolkit/Button.java
@@ -7,37 +7,26 @@ import java.util.ArrayList;
 
 import org.joml.Vector2f;
 import org.lwjgl.input.Mouse;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL13;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
-import org.newdawn.slick.opengl.Texture;
 
-import gameEngine.models.RawModel;
 import gameEngine.renderEngine.Loader;
 
 /**
  * 
+ * A UIElement that can perform an action when clicked. This is set by adding
+ * ActionListener to the button.
+ * 
  * @author Reece Bennett
  *
  */
-public class Button {
+public class Button extends UIElement {
 
-  private Texture texture;
-
-  private Vector2f position;
   private Rectangle bounds;
-
-  private Loader loader;
 
   private ArrayList<ActionListener> listeners;
   private boolean pressed;
 
   public Button(Loader loader, String fileName, Vector2f position) {
-    this.loader = loader;
-    this.texture = loader.loadTextureTex(fileName);
-    this.position = position;
+    super(loader, fileName, position);
 
     bounds = new Rectangle();
     bounds.x = (int) position.x;
@@ -53,6 +42,7 @@ public class Button {
     listeners.add(listener);
   }
 
+  @Override
   public void update() {
     if (bounds.contains(Mouse.getX(), Mouse.getY()) && Mouse.isButtonDown(0) && !pressed) {
       pressed = true;
@@ -65,43 +55,6 @@ public class Button {
         a.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "released"));
       }
     }
-  }
-
-  public void render(Vector2f containerPos, Vector2f containerSize) {
-    int w = Display.getWidth();
-    int h = Display.getHeight();
-
-    // Calculate the position of the quad to draw onto and its texture
-    // coordinates
-    Vector2f topLeft = new Vector2f((2 * (position.x + containerPos.x)) / (float) w - 1,
-        -((2 * (position.y + containerPos.y)) / (float) h - 1));
-
-    Vector2f botRight = new Vector2f(
-        (2 * (position.x + containerPos.x + texture.getImageWidth())) / (float) w - 1,
-        -((2 * (position.y + containerPos.y + texture.getImageHeight())) / (float) h - 1));
-
-    Vector2f texSize = new Vector2f((float) texture.getImageWidth() / texture.getTextureWidth(),
-        (float) texture.getImageHeight() / texture.getTextureHeight());
-
-    float[] positions = { topLeft.x, topLeft.y, topLeft.x, botRight.y, botRight.x, topLeft.y,
-        botRight.x, botRight.y };
-    float[] textureCoords = { 0, 0, 0, texSize.y, texSize.x, 0, texSize.x, texSize.y };
-
-    // Load the quad into a VAO
-    RawModel quad = loader.loadToVAO(positions, textureCoords);
-
-    // Do render things
-    GL30.glBindVertexArray(quad.getVaoID());
-    GL20.glEnableVertexAttribArray(0);
-    GL20.glEnableVertexAttribArray(1);
-    GL13.glActiveTexture(GL13.GL_TEXTURE0);
-    GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture.getTextureID());
-
-    GL11.glDrawArrays(GL11.GL_TRIANGLE_STRIP, 0, quad.getVertexCount());
-
-    GL20.glDisableVertexAttribArray(0);
-    GL20.glDisableVertexAttribArray(1);
-    GL30.glBindVertexArray(0);
   }
 
 }
--- src/uiToolkit/Container.java
+++ src/uiToolkit/Container.java
@@ -3,86 +3,50 @@ package uiToolkit;
 import java.util.ArrayList;
 
 import org.joml.Vector2f;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL13;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
-import org.newdawn.slick.opengl.Texture;
 
-import gameEngine.models.RawModel;
 import gameEngine.renderEngine.Loader;
 
 /**
  * 
- * A UI object that contains other UI objects such as buttons and other
+ * A UIElement that can contain other UIElement such as buttons and other
  * containers.
  * 
  * @author Reece Bennett
  *
  */
-public class Container {
+public class Container extends UIElement {
 
-  private Texture texture;
-  private ArrayList<Button> buttons;
-
-  private Vector2f position;
-
-  private Loader loader;
+  private ArrayList<UIElement> children;
 
   public Container(Loader loader, String fileName, Vector2f position) {
-    this.loader = loader;
-    this.texture = loader.loadTextureTex(fileName);
-    this.position = position;
+    super(loader, fileName, position);
 
-    buttons = new ArrayList<>();
+    children = new ArrayList<>();
   }
 
-  public void add(Button button) {
-    buttons.add(button);
+  public void add(UIElement child) {
+    children.add(child);
   }
 
+  @Override
   public void update() {
-    for (Button b : buttons) {
-      b.update();
+    for (UIElement e : children) {
+      e.update();
     }
   }
 
+  @Override
   public void render() {
-    int w = Display.getWidth();
-    int h = Display.getHeight();
+    render(new Vector2f(), new Vector2f());
+  }
 
-    // Calculate the position of the quad to draw onto and its texture coordinates
-    Vector2f topLeft = new Vector2f((2 * position.x) / (float) w - 1,
-        -((2 * position.y) / (float) h - 1));
-    Vector2f botRight = new Vector2f((2 * (position.x + texture.getImageWidth())) / (float) w - 1,
-        -((2 * (position.y + texture.getImageHeight())) / (float) h - 1));
-    Vector2f texSize = new Vector2f((float) texture.getImageWidth() / texture.getTextureWidth(),
-        (float) texture.getImageHeight() / texture.getTextureHeight());
-
-    float[] positions = { topLeft.x, topLeft.y, topLeft.x, botRight.y, botRight.x, topLeft.y,
-        botRight.x, botRight.y };
-    float[] textureCoords = { 0, 0, 0, texSize.y, texSize.x, 0, texSize.x, texSize.y };
-
-    // Load the quad into a VAO
-    RawModel quad = loader.loadToVAO(positions, textureCoords);
-
-    // Do render things
-    GL30.glBindVertexArray(quad.getVaoID());
-    GL20.glEnableVertexAttribArray(0);
-    GL20.glEnableVertexAttribArray(1);
-    GL13.glActiveTexture(GL13.GL_TEXTURE0);
-    GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture.getTextureID());
-
-    GL11.glDrawArrays(GL11.GL_TRIANGLE_STRIP, 0, quad.getVertexCount());
-
-    GL20.glDisableVertexAttribArray(0);
-    GL20.glDisableVertexAttribArray(1);
-    GL30.glBindVertexArray(0);
+  @Override
+  public void render(Vector2f containerPos, Vector2f containerSize) {
+    super.render(containerPos, containerSize);
 
     // Render all children of this container
-    for (Button b : buttons) {
-      b.render(position, new Vector2f(texture.getImageWidth(), texture.getImageHeight()));
+    for (UIElement e : children) {
+      e.render(position, new Vector2f(texture.getImageWidth(), texture.getImageHeight()));
     }
   }
 
