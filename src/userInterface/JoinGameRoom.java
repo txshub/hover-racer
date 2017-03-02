b7a7c349031964a6228077be3acbfc1e48f88207
aig530 <aig530@student.bham.ac.uk>
2017-03-02 14:05:25 +0000

    dev updates

--- src/userInterface/JoinGameRoom.java
+++ src/userInterface/JoinGameRoom.java
@@ -18,11 +18,26 @@ public class JoinGameRoom extends GridPane {
 	private ArrayList<GameNameNumber> gameRoomList;
 	private Client client;
 	
-	public JoinGameRoom(Client client) {
+	public JoinGameRoom(Client client) throws IOException {
 	
 		this.client = client;
+		this.gameRoomList = client.requestAllGames(); //this gets the actual list of game rooms
 		
-		this.gameRoomList = client.getAllGames(); //this gets the actual list of game rooms
+		//Display the array of game rooms
+		for(int i=0; i< gameRoomList.size(); i++){
+			
+		//	gameRoomList.get(i).
+			
+		}
+		
+		MenuButton refresh = new MenuButton("REFRESH");
+		refresh.setOnMouseClicked(event -> {
+			
+			this.gameRoomList = client.requestAllGames();
+			
+			
+			
+		});
 		
 		
 	}
