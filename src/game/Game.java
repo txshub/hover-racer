c061f5bc5e025c6a11a876696fefb6fcd04602f5
Reece Bennett <reece.bennett1@gmail.com>
2017-03-09 16:04:34 +0000

    Re-write track mesh generator
    Not fully working, no normals yet, no defining vertices for edge of barrier correctly and seems to be some faces that are defined inside out.

--- src/game/Game.java
+++ src/game/Game.java
@@ -222,11 +222,21 @@ public class Game implements GameInt {
     return data;
   }
 
+  /**
+   * Create a track model from a seed
+   * 
+   * @param seed
+   * @return
+   * @author Reece Bennett
+   */
   private TexturedModel createTrackModel(long seed) {
     float trackHeight = 1;
+    float barrierHeight = 2;
 
-    float[] vertices = new float[trackPoints.size() * 5 * 3];
-    int[] indices = new int[trackPoints.size() * 5 * 8];
+    // 6 vertices for each track point, 3 components for each vertex
+    float[] vertices = new float[trackPoints.size() * 6 * 3];
+    // 10 triangles for each track point, 3 vertices per triangle
+    int[] indices = new int[trackPoints.size() * 10 * 3];
     float[] texCoords = new float[indices.length];
     float[] normals = new float[vertices.length];
 
@@ -234,167 +244,266 @@ public class Game implements GameInt {
     for (int i = 0; i < texCoords.length; i++) {
       texCoords[i] = 0;
     }
+    
+    for (int i = 0; i < normals.length; i += 3) {
+      addToArray(new Vector3f(0, 1, 0), normals, i);
+    }
 
-    for (int i = 0; i <= trackPoints.size(); i++) {
+    // Populate vertex array
+    for (int i = 0; i < trackPoints.size(); i++) {
+      TrackPoint curPoint = trackPoints.get(i);
 
-      if (i < trackPoints.size()) {
+      // If we are at the first point the previous is the last point
+      int prev = (i == 0) ? trackPoints.size() - 1 : i - 1;
+      TrackPoint prevPoint = trackPoints.get(prev);
 
-        TrackPoint curPoint = trackPoints.get(i);
+      // If we are at the last point the next is the first point
+      int next = (i == trackPoints.size() - 1) ? 0 : i + 1;
+      TrackPoint nextPoint = trackPoints.get(next);
 
-        // If we are at the first point the previous is the last point
-        int p = i == 0 ? trackPoints.size() - 1 : i - 1;
-        TrackPoint prevPoint = trackPoints.get(p);
+      // Find the line between previous and next point for direction of this
+      // slice
+      Vector2f dirVec = new Vector2f(nextPoint).sub(prevPoint).normalize();
 
-        // If we are at the last point the next is the first point
-        p = (i + 1) >= trackPoints.size() ? 0 : i + 1;
-        TrackPoint nextPoint = trackPoints.get(p);
+      // Calculate the perpendicular vectors
+      Vector2f left = new Vector2f(dirVec.y, -dirVec.x).mul(curPoint.getWidth() / 2);
+      Vector2f right = new Vector2f(-dirVec.y, dirVec.x).mul(curPoint.getWidth() / 2);
 
-        // System.out.println("Cur: " + curPoint + " Next: " + nextPoint
-        // + " " + (nextPoint.getX() - curPoint.getX()));
+      // Apply the offsets to the center point
+      Vector3f centerPoint = new Vector3f(curPoint.x, trackHeight, curPoint.y);
+      Vector3f leftPoint = new Vector3f(centerPoint).add(left.x, 0, left.y);
+      Vector3f rightPoint = new Vector3f(centerPoint).add(right.x, 0, right.y);
+      
+      // Create barrier points
+      Vector3f lBarrierT = new Vector3f(leftPoint).add(0, barrierHeight, 0);
+      Vector3f rBarrierT = new Vector3f(rightPoint).add(0, barrierHeight, 0);
+      Vector3f lBarrierB = new Vector3f(leftPoint).add(0, -trackHeight, 0);
+      Vector3f rBarrierB = new Vector3f(rightPoint).add(0, -trackHeight, 0);
+      
+      addToArray(lBarrierB, vertices, i * 18 + 0);
+      addToArray(lBarrierT, vertices, i * 18 + 3);
+      addToArray(leftPoint, vertices, i * 18 + 6);
+      addToArray(rightPoint, vertices, i * 18 + 9);
+      addToArray(rBarrierT, vertices, i * 18 + 12);
+      addToArray(rBarrierB, vertices, i * 18 + 15);
+      
+      // TODO Normals
+    }
+    
+    // Populate indices
+    for (int i = 0; i < trackPoints.size(); i++) {
+      int n = i * 5;
+      int offset = i * 30;
+      
+      if (i < trackPoints.size() - 1) {
+        addToArray(n, n+7, n+6, indices, offset);
+        addToArray(n, n+1, n+7, indices, offset+3);
 
-        Vector2f dirFromPrev = new Vector2f(curPoint.getX() - prevPoint.getX(),
-            curPoint.getY() - prevPoint.getY());
+        addToArray(n+1, n+8, n+7, indices, offset+6);
+        addToArray(n+1, n+2, n+8, indices, offset+9);
 
-        Vector2f dirToNext = new Vector2f(nextPoint.getX() - curPoint.getX(),
-            nextPoint.getY() - curPoint.getY());
+        addToArray(n+2, n+9, n+8, indices, offset+12);
+        addToArray(n+2, n+3, n+9, indices, offset+15);
 
-        Vector2f dirVec = dirFromPrev.add(dirToNext).normalize();
+        addToArray(n+3, n+10, n+9, indices, offset+18);
+        addToArray(n+3, n+4, n+10, indices, offset+21);
 
-        Vector2f left = new Vector2f(dirVec.y, -dirVec.x).mul(curPoint.getWidth() / 2);
-        Vector2f right = new Vector2f(-dirVec.y, dirVec.x).mul(curPoint.getWidth() / 2);
-        Vector2f leftNorm = new Vector2f(left).normalize();
-        Vector2f rightNorm = new Vector2f(right).normalize();
+        addToArray(n+4, n+11, n+10, indices, offset+24);
+        addToArray(n+4, n+5, n+11, indices, offset+27);
+      
+      } else {
+        // If we are at the last point we should join to the start of the track
+        addToArray(n, 2, 0, indices, offset);
+        addToArray(n, n+1, 2, indices, offset+3);
 
-        // System.out.println("Dir: " + dirVec + " Left: " +
-        // left.length() + " Right: " + right.length());
+        addToArray(n+1, 3, 2, indices, offset+6);
+        addToArray(n+1, n+2, 3, indices, offset+9);
 
-        Vector3f normal;
+        addToArray(n+2, 4, 3, indices, offset+12);
+        addToArray(n+2, n+3, 4, indices, offset+15);
 
-        // Top Left
-        vertices[i * 15] = curPoint.getX() + left.x;
-        vertices[i * 15 + 1] = trackHeight;
-        vertices[i * 15 + 2] = curPoint.getY() + left.y;
-        normal = new Vector3f(leftNorm.x, 1, leftNorm.y).normalize();
-        normals[i * 15 + 0] = normal.x;
-        normals[i * 15 + 1] = normal.y;
-        normals[i * 15 + 2] = normal.z;
+        addToArray(n+3, 5, 4, indices, offset+18);
+        addToArray(n+3, n+4, 5, indices, offset+21);
 
-        // Bottom Left
-        vertices[i * 15 + 3] = curPoint.getX() + left.x;
-        vertices[i * 15 + 4] = 0;
-        vertices[i * 15 + 5] = curPoint.getY() + left.y;
-        normal = new Vector3f(leftNorm.x, -1, leftNorm.y).normalize();
-        normals[i * 15 + 3] = normal.x;
-        normals[i * 15 + 4] = normal.y;
-        normals[i * 15 + 5] = normal.z;
-
-        // Top Center
-        vertices[i * 15 + 6] = curPoint.getX();
-        vertices[i * 15 + 7] = trackHeight;
-        vertices[i * 15 + 8] = curPoint.getY();
-        normal = new Vector3f(0, 1, 0).normalize();
-        normals[i * 15 + 6] = normal.x;
-        normals[i * 15 + 7] = normal.y;
-        normals[i * 15 + 8] = normal.z;
-
-        // Top Right
-        vertices[i * 15 + 9] = curPoint.getX() + right.x;
-        vertices[i * 15 + 10] = trackHeight;
-        vertices[i * 15 + 11] = curPoint.getY() + right.y;
-        normal = new Vector3f(rightNorm.x, 1, rightNorm.y).normalize();
-        normals[i * 15 + 9] = normal.x;
-        normals[i * 15 + 10] = normal.y;
-        normals[i * 15 + 11] = normal.z;
-
-        // Bottom Right
-        vertices[i * 15 + 12] = curPoint.getX() + right.x;
-        vertices[i * 15 + 13] = 0;
-        vertices[i * 15 + 14] = curPoint.getY() + right.y;
-        normal = new Vector3f(rightNorm.x, -1, rightNorm.y).normalize();
-        normals[i * 15 + 12] = normal.x;
-        normals[i * 15 + 13] = normal.y;
-        normals[i * 15 + 14] = normal.z;
-
-        // System.out.println("Left: " + vertices[i*9] + " " +
-        // vertices[i*9+1] + " " + vertices[i*9+2]
-        // + " Center: " + vertices[i*9+3] + " " + vertices[i*9+4] + " "
-        // + vertices[i*9+5]
-        // + " Right: " + vertices[i*9+6] + " " + vertices[i*9+7] + " "
-        // + vertices[i*9+8]);
-      }
-
-      int k = i * 5;
-      if (i > 0 && i < trackPoints.size()) {
-
-        indices[(i - 1) * 24 + 0] = k - 4;
-        indices[(i - 1) * 24 + 1] = k;
-        indices[(i - 1) * 24 + 2] = k + 1;
-
-        indices[(i - 1) * 24 + 3] = k - 5;
-        indices[(i - 1) * 24 + 4] = k;
-        indices[(i - 1) * 24 + 5] = k - 4;
-
-        indices[(i - 1) * 24 + 6] = k + 2;
-        indices[(i - 1) * 24 + 7] = k;
-        indices[(i - 1) * 24 + 8] = k - 5;
-
-        indices[(i - 1) * 24 + 9] = k - 3;
-        indices[(i - 1) * 24 + 10] = k + 2;
-        indices[(i - 1) * 24 + 11] = k - 5;
-
-        indices[(i - 1) * 24 + 12] = k - 3;
-        indices[(i - 1) * 24 + 13] = k - 2;
-        indices[(i - 1) * 24 + 14] = k + 2;
-
-        indices[(i - 1) * 24 + 15] = k - 2;
-        indices[(i - 1) * 24 + 16] = k + 3;
-        indices[(i - 1) * 24 + 17] = k + 2;
-
-        indices[(i - 1) * 24 + 18] = k - 1;
-        indices[(i - 1) * 24 + 19] = k + 3;
-        indices[(i - 1) * 24 + 20] = k - 2;
-
-        indices[(i - 1) * 24 + 21] = k - 1;
-        indices[(i - 1) * 24 + 22] = k + 4;
-        indices[(i - 1) * 24 + 23] = k + 3;
-      } else if (i > 0) {
-
-        indices[(i - 1) * 24 + 0] = k - 4;
-        indices[(i - 1) * 24 + 1] = 0;
-        indices[(i - 1) * 24 + 2] = 0 + 1;
-
-        indices[(i - 1) * 24 + 3] = k - 5;
-        indices[(i - 1) * 24 + 4] = 0;
-        indices[(i - 1) * 24 + 5] = k - 4;
-
-        indices[(i - 1) * 24 + 6] = 0 + 2;
-        indices[(i - 1) * 24 + 7] = 0;
-        indices[(i - 1) * 24 + 8] = k - 5;
-
-        indices[(i - 1) * 24 + 9] = k - 3;
-        indices[(i - 1) * 24 + 10] = 0 + 2;
-        indices[(i - 1) * 24 + 11] = k - 5;
-
-        indices[(i - 1) * 24 + 12] = k - 3;
-        indices[(i - 1) * 24 + 13] = k - 2;
-        indices[(i - 1) * 24 + 14] = 0 + 2;
-
-        indices[(i - 1) * 24 + 15] = k - 2;
-        indices[(i - 1) * 24 + 16] = 0 + 3;
-        indices[(i - 1) * 24 + 17] = 0 + 2;
-
-        indices[(i - 1) * 24 + 18] = k - 1;
-        indices[(i - 1) * 24 + 19] = 0 + 3;
-        indices[(i - 1) * 24 + 20] = k - 2;
-
-        indices[(i - 1) * 24 + 21] = k - 1;
-        indices[(i - 1) * 24 + 22] = 0 + 4;
-        indices[(i - 1) * 24 + 23] = 0 + 3;
+        addToArray(n+4, 6, 5, indices, offset+24);
+        addToArray(n+4, n+5, 5, indices, offset+27);
       }
     }
+
+    // for (int i = 0; i <= trackPoints.size(); i++) {
+    //
+    // if (i < trackPoints.size()) {
+    //
+    // TrackPoint curPoint = trackPoints.get(i);
+    //
+    // // If we are at the first point the previous is the last point
+    // int p = i == 0 ? trackPoints.size() - 1 : i - 1;
+    // TrackPoint prevPoint = trackPoints.get(p);
+    //
+    // // If we are at the last point the next is the first point
+    // p = (i + 1) >= trackPoints.size() ? 0 : i + 1;
+    // TrackPoint nextPoint = trackPoints.get(p);
+    //
+    // // System.out.println("Cur: " + curPoint + " Next: " + nextPoint
+    // // + " " + (nextPoint.getX() - curPoint.getX()));
+    //
+    // Vector2f dirFromPrev = new Vector2f(curPoint.getX() - prevPoint.getX(),
+    // curPoint.getY() - prevPoint.getY());
+    //
+    // Vector2f dirToNext = new Vector2f(nextPoint.getX() - curPoint.getX(),
+    // nextPoint.getY() - curPoint.getY());
+    //
+    // Vector2f dirVec = dirFromPrev.add(dirToNext).normalize();
+    //
+    // Vector2f left = new Vector2f(dirVec.y, -dirVec.x).mul(curPoint.getWidth()
+    // / 2);
+    // Vector2f right = new Vector2f(-dirVec.y,
+    // dirVec.x).mul(curPoint.getWidth() / 2);
+    // Vector2f leftNorm = new Vector2f(left).normalize();
+    // Vector2f rightNorm = new Vector2f(right).normalize();
+    //
+    // // System.out.println("Dir: " + dirVec + " Left: " +
+    // // left.length() + " Right: " + right.length());
+    //
+    // Vector3f normal;
+    //
+    // // Top Left
+    // vertices[i * 15] = curPoint.getX() + left.x;
+    // vertices[i * 15 + 1] = trackHeight;
+    // vertices[i * 15 + 2] = curPoint.getY() + left.y;
+    // normal = new Vector3f(leftNorm.x, 1, leftNorm.y).normalize();
+    // normals[i * 15 + 0] = normal.x;
+    // normals[i * 15 + 1] = normal.y;
+    // normals[i * 15 + 2] = normal.z;
+    //
+    // // Bottom Left
+    // vertices[i * 15 + 3] = curPoint.getX() + left.x;
+    // vertices[i * 15 + 4] = 0;
+    // vertices[i * 15 + 5] = curPoint.getY() + left.y;
+    // normal = new Vector3f(leftNorm.x, -1, leftNorm.y).normalize();
+    // normals[i * 15 + 3] = normal.x;
+    // normals[i * 15 + 4] = normal.y;
+    // normals[i * 15 + 5] = normal.z;
+    //
+    // // Top Center
+    // vertices[i * 15 + 6] = curPoint.getX();
+    // vertices[i * 15 + 7] = trackHeight;
+    // vertices[i * 15 + 8] = curPoint.getY();
+    // normal = new Vector3f(0, 1, 0).normalize();
+    // normals[i * 15 + 6] = normal.x;
+    // normals[i * 15 + 7] = normal.y;
+    // normals[i * 15 + 8] = normal.z;
+    //
+    // // Top Right
+    // vertices[i * 15 + 9] = curPoint.getX() + right.x;
+    // vertices[i * 15 + 10] = trackHeight;
+    // vertices[i * 15 + 11] = curPoint.getY() + right.y;
+    // normal = new Vector3f(rightNorm.x, 1, rightNorm.y).normalize();
+    // normals[i * 15 + 9] = normal.x;
+    // normals[i * 15 + 10] = normal.y;
+    // normals[i * 15 + 11] = normal.z;
+    //
+    // // Bottom Right
+    // vertices[i * 15 + 12] = curPoint.getX() + right.x;
+    // vertices[i * 15 + 13] = 0;
+    // vertices[i * 15 + 14] = curPoint.getY() + right.y;
+    // normal = new Vector3f(rightNorm.x, -1, rightNorm.y).normalize();
+    // normals[i * 15 + 12] = normal.x;
+    // normals[i * 15 + 13] = normal.y;
+    // normals[i * 15 + 14] = normal.z;
+    //
+    // // System.out.println("Left: " + vertices[i*9] + " " +
+    // // vertices[i*9+1] + " " + vertices[i*9+2]
+    // // + " Center: " + vertices[i*9+3] + " " + vertices[i*9+4] + " "
+    // // + vertices[i*9+5]
+    // // + " Right: " + vertices[i*9+6] + " " + vertices[i*9+7] + " "
+    // // + vertices[i*9+8]);
+    // }
+    //
+    // int k = i * 5;
+    // if (i > 0 && i < trackPoints.size()) {
+    //
+    // indices[(i - 1) * 24 + 0] = k - 4;
+    // indices[(i - 1) * 24 + 1] = k;
+    // indices[(i - 1) * 24 + 2] = k + 1;
+    //
+    // indices[(i - 1) * 24 + 3] = k - 5;
+    // indices[(i - 1) * 24 + 4] = k;
+    // indices[(i - 1) * 24 + 5] = k - 4;
+    //
+    // indices[(i - 1) * 24 + 6] = k + 2;
+    // indices[(i - 1) * 24 + 7] = k;
+    // indices[(i - 1) * 24 + 8] = k - 5;
+    //
+    // indices[(i - 1) * 24 + 9] = k - 3;
+    // indices[(i - 1) * 24 + 10] = k + 2;
+    // indices[(i - 1) * 24 + 11] = k - 5;
+    //
+    // indices[(i - 1) * 24 + 12] = k - 3;
+    // indices[(i - 1) * 24 + 13] = k - 2;
+    // indices[(i - 1) * 24 + 14] = k + 2;
+    //
+    // indices[(i - 1) * 24 + 15] = k - 2;
+    // indices[(i - 1) * 24 + 16] = k + 3;
+    // indices[(i - 1) * 24 + 17] = k + 2;
+    //
+    // indices[(i - 1) * 24 + 18] = k - 1;
+    // indices[(i - 1) * 24 + 19] = k + 3;
+    // indices[(i - 1) * 24 + 20] = k - 2;
+    //
+    // indices[(i - 1) * 24 + 21] = k - 1;
+    // indices[(i - 1) * 24 + 22] = k + 4;
+    // indices[(i - 1) * 24 + 23] = k + 3;
+    // } else if (i > 0) {
+    //
+    // indices[(i - 1) * 24 + 0] = k - 4;
+    // indices[(i - 1) * 24 + 1] = 0;
+    // indices[(i - 1) * 24 + 2] = 0 + 1;
+    //
+    // indices[(i - 1) * 24 + 3] = k - 5;
+    // indices[(i - 1) * 24 + 4] = 0;
+    // indices[(i - 1) * 24 + 5] = k - 4;
+    //
+    // indices[(i - 1) * 24 + 6] = 0 + 2;
+    // indices[(i - 1) * 24 + 7] = 0;
+    // indices[(i - 1) * 24 + 8] = k - 5;
+    //
+    // indices[(i - 1) * 24 + 9] = k - 3;
+    // indices[(i - 1) * 24 + 10] = 0 + 2;
+    // indices[(i - 1) * 24 + 11] = k - 5;
+    //
+    // indices[(i - 1) * 24 + 12] = k - 3;
+    // indices[(i - 1) * 24 + 13] = k - 2;
+    // indices[(i - 1) * 24 + 14] = 0 + 2;
+    //
+    // indices[(i - 1) * 24 + 15] = k - 2;
+    // indices[(i - 1) * 24 + 16] = 0 + 3;
+    // indices[(i - 1) * 24 + 17] = 0 + 2;
+    //
+    // indices[(i - 1) * 24 + 18] = k - 1;
+    // indices[(i - 1) * 24 + 19] = 0 + 3;
+    // indices[(i - 1) * 24 + 20] = k - 2;
+    //
+    // indices[(i - 1) * 24 + 21] = k - 1;
+    // indices[(i - 1) * 24 + 22] = 0 + 4;
+    // indices[(i - 1) * 24 + 23] = 0 + 3;
+    // }
+    // }
     return new TexturedModel(loader.loadToVAO(vertices, texCoords, normals, indices),
         new ModelTexture(loader.loadTexture("new/TrackTexture")));
 
+  }
+  
+  private void addToArray(Vector3f vector, float[] array, int offset) {
+    array[offset + 0] = vector.x;
+    array[offset + 1] = vector.y;
+    array[offset + 2] = vector.z;
+  }
+  
+  private void addToArray(int first, int second, int third, int[] array, int offset) {
+    array[offset++] = first;
+    array[offset++] = second;
+    array[offset++] = third;
   }
 
   private static void sortLights(List<Light> lights, Vector3f currentPosition) {
