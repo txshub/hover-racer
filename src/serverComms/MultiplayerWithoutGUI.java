a0942395a537c1eb881cb6ac6304ced947b10ec5
mxb551 <mxb551@cs.bham.ac.uk>
2017-03-12 22:45:23 +0000

    Simple class for testing multiplayer

---
+++ src/serverComms/MultiplayerWithoutGUI.java
@@ -0,0 +1,54 @@
+package serverComms;
+
+import java.io.IOException;
+
+import clientComms.Client;
+import physics.placeholders.DataGenerator;
+
+public class MultiplayerWithoutGUI {
+
+	private static final int PORT = 4444;
+	private static final String MACHINE_NAME = "localhost";
+	private static Client client;
+	private static GameRoom gameRoom;
+	private static final int GAME_ID = 50;
+	private static final long SEED = 42;
+	private static final int MAX_PLAYERS = 3;
+	private static final int LAPS = 2;
+	private static final String LOBBY_NAME = "Testonium";
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		// Start the server
+		Lobby serverLobby = new Lobby(4444);
+		ServerComm server = new ServerComm(4444, serverLobby);
+		server.start();
+
+		// Create and connect the user
+		String usr = "Teston";
+		int portNo = PORT;
+		String machineName = MACHINE_NAME;
+
+		client = new Client(usr, portNo, machineName, null);
+		client.start();
+
+		// Create a game room
+		gameRoom = client.createGame(SEED, MAX_PLAYERS, LAPS, LOBBY_NAME, DataGenerator.basicShipSetup(client.clientName));
+
+		// Join a game room
+		// gameRoom = client.joinGame(GAME_ID, DataGenerator.basicShipSetup(usr));
+
+		// Start the game
+		client.startGame();
+
+		/* long closeAt = System.nanoTime() + 10000000000l;
+		 * while (System.nanoTime() < closeAt) {
+		 * Thread.sleep(100);
+		 * }
+		 * System.out.println("---------------EXITING-----------------"); */
+
+	}
+
+
+
+}
