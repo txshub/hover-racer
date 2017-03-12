60cb15dffed9e1636fa025bdce3fd1f468c40383
mxb551 <mxb551@cs.bham.ac.uk>
2017-03-12 22:41:27 +0000

    Fixed Simon's bug where the sender would never sleep

--- src/serverComms/UpdateAllUsers.java
+++ src/serverComms/UpdateAllUsers.java
@@ -4,38 +4,38 @@ import java.util.ArrayList;
 
 public class UpdateAllUsers extends Thread {
 
-  public volatile boolean run = true;
+	public volatile boolean run = true;
 
-  final long refreshRate = (long)(1.0 / 60.0) * 1000000; // Refresh Rate in nanos
+	final long refreshRate = (long) ((float) 1.0 / 60.0) * 1000000; // Refresh Rate in nanos
 
-  private ArrayList<CommQueue> queues;
-  private GameRoom room;
+	private ArrayList<CommQueue> queues;
+	private GameRoom room;
 
-  public UpdateAllUsers(ArrayList<CommQueue> queues, GameRoom room) {
-    this.queues = queues;
-    this.room = room;
-  }
+	public UpdateAllUsers(ArrayList<CommQueue> queues, GameRoom room) {
+		this.queues = queues;
+		this.room = room;
+	}
 
-  @Override
-  public void run() {
-    while (run) {
-      long timeStarting = System.nanoTime();
-      byte[] data = room.getShipPositions();
-      for (CommQueue queue : queues) {
-        queue.offer(new ByteArrayByte(data, ServerComm.FULLPOSITIONUPDATE));
-      }
-      long timeAfter = System.nanoTime();
-      try {
-    	  System.out.println(timeStarting + ", " + timeAfter);
-        Thread.sleep(Math.max(0, (refreshRate - timeAfter + timeStarting) / 1000)); // Sleep
-                                                                       // for
-                                                                       // 1/60th
-                                                                       // of a
-                                                                       // second
-      } catch (InterruptedException e) {
-        // Needed for compilation, won't actually happen
-      }
-    }
-  }
+	@Override
+	public void run() {
+		while (run) {
+			long timeStarting = System.nanoTime();
+			byte[] data = room.getShipPositions();
+			for (CommQueue queue : queues) {
+				queue.offer(new ByteArrayByte(data, ServerComm.FULLPOSITIONUPDATE));
+			}
+			long timeAfter = System.nanoTime();
+			try {
+				// System.out.println(timeStarting + ", " + timeAfter);
+				Thread.sleep(Math.max(0, (refreshRate - timeAfter + timeStarting) / 1000)); // Sleep
+				// for
+				// 1/60th
+				// of a
+				// second
+			} catch (InterruptedException e) {
+				// Needed for compilation, won't actually happen
+			}
+		}
+	}
 
 }
