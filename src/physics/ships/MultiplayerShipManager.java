2941e86bd391b46832be91cb8c96dd643c1dfda9
mxb551 <mxb551@cs.bham.ac.uk>
2017-03-21 15:51:55 +0000

    Added javadocs

--- src/physics/ships/MultiplayerShipManager.java
+++ src/physics/ships/MultiplayerShipManager.java
@@ -23,12 +23,10 @@ import physics.support.ShipSounds;
 import trackDesign.TrackPoint;
 import upgrades.ShipTemplate;
 
-/**
- * A class that manages ship on client side while in multiplayer mode. Handles
+/** A class that manages ship on client side while in multiplayer mode. Handles
  * creating them, updating and communicating with the server.
  * 
- * @author Maciej Bogacki
- */
+ * @author Maciej Bogacki */
 public class MultiplayerShipManager implements ServerShipProvider {
 
 	private static final int SHIP_PACKET_LENGTH = 49;
@@ -38,25 +36,29 @@ public class MultiplayerShipManager implements ServerShipProvider {
 	private ShipSounds sounds;
 	private ArrayList<TrackPoint> track;
 
+	/** Creates a new ship manager.
+	 * 
+	 * @param data Data received from the server during setup
+	 * @param ground Ground for ships to levitate over
+	 * @param input Input for the player
+	 * @param loader Loader object for loading textures
+	 * @param track Track to generate the barriers from */
 	public MultiplayerShipManager(RaceSetupData data, GroundProvider ground, InputController input, Loader loader,
-			ArrayList<TrackPoint> track) {
-		this(data.getYourId(), input, makeModels(data.getModels(), data.getTextures(), loader),
-				data.getStartingPositions(), data.getStartingOrientation(), data.getStats(), ground, track);
+		ArrayList<TrackPoint> track) {
+		this(data.getYourId(), input, makeModels(data.getModels(), data.getTextures(), loader), data.getStartingPositions(),
+			data.getStartingOrientation(), data.getStats(), ground, track);
 	}
 
-	public MultiplayerShipManager(byte playerId, InputController input, List<TexturedModel> models,
-			List<Vector3f> startingPositions, Vector3f startingOrientation, List<ShipTemplate> stats,
-			GroundProvider ground, ArrayList<TrackPoint> track) {
+	private MultiplayerShipManager(byte playerId, InputController input, List<TexturedModel> models, List<Vector3f> startingPositions,
+		Vector3f startingOrientation, List<ShipTemplate> stats, GroundProvider ground, ArrayList<TrackPoint> track) {
 		this.packets = new HashMap<Byte, byte[]>();
 		this.track = track;
 
 		// Create all RemoteShips
 		this.remotes = new ArrayList<Ship>();
 		for (byte id = 0; id < models.size(); id++) {
-			if (id == playerId)
-				continue; // Don't give them player's id
-			RemoteShip remoteShip = new RemoteShip(id, models.get(id), startingPositions.get(id), ground, stats.get(id),
-					track, this);
+			if (id == playerId) continue; // Don't give them player's id
+			RemoteShip remoteShip = new RemoteShip(id, models.get(id), startingPositions.get(id), ground, stats.get(id), track, this);
 			remoteShip.setRotation(startingOrientation);
 			remotes.add(remoteShip);
 			System.out.println("Created: " + remoteShip);
@@ -64,8 +66,8 @@ public class MultiplayerShipManager implements ServerShipProvider {
 		// Tell ships about each other
 		remotes.forEach(r -> r.addOtherShips(remotes));
 		// Finally create the player
-		this.player = new PlayerShip(playerId, models.get(playerId), startingPositions.get(playerId), remotes, ground,
-				stats.get(playerId), track, input);
+		this.player = new PlayerShip(playerId, models.get(playerId), startingPositions.get(playerId), remotes, ground, stats.get(playerId),
+			track, input);
 		player.setRotation(startingOrientation);
 		remotes.forEach(r -> r.addOtherShip(player));
 		// Create sounds
@@ -75,22 +77,20 @@ public class MultiplayerShipManager implements ServerShipProvider {
 
 	@Deprecated
 	public MultiplayerShipManager(byte playerId, InputController input, TexturedModel playerTexture,
-			Collection<TexturedModel> otherTextures, ArrayList<Vector3f> startingPositions, GroundProvider ground) {
+		Collection<TexturedModel> otherTextures, ArrayList<Vector3f> startingPositions, GroundProvider ground) {
 		this.packets = new HashMap<Byte, byte[]>();
 
 		// Create all RemoteShips
 		this.remotes = new ArrayList<Ship>();
 		byte id = 0;
 		for (TexturedModel texturedModel : otherTextures) {
-			if (id == playerId)
-				id++; // Don't give them player's id
+			if (id == playerId) id++; // Don't give them player's id
 			remotes.add(new RemoteShip(id, texturedModel, startingPositions.get(id++), ground, null, null, this));
 		}
 		// Tell ships about each other
 		remotes.forEach(r -> r.addOtherShips(remotes));
 		// Finally create the player
-		this.player = new PlayerShip(playerId, playerTexture, startingPositions.get(playerId), remotes, ground, null,
-				null, input);
+		this.player = new PlayerShip(playerId, playerTexture, startingPositions.get(playerId), remotes, ground, null, null, input);
 		remotes.forEach(r -> r.addOtherShip(player));
 	}
 
@@ -109,24 +109,19 @@ public class MultiplayerShipManager implements ServerShipProvider {
 		return player;
 	}
 
-	/**
-	 * Starts the race, allowing input through. This is when racing actually
-	 * starts, so probably a few seconds after the game is loaded.
-	 */
+	/** Starts the race, allowing input through. This is when racing actually
+	 * starts, so probably a few seconds after the game is loaded. */
 	public void startRace() {
 		player.start();
 		remotes.forEach(r -> r.start());
-		// TODO other effects when starting race?
 	}
 
-	/**
-	 * Adds a client-side AI to a multiplayer game, only to be used for testing.
+	/** Adds a client-side AI to a multiplayer game, only to be used for testing.
 	 * The server won't know about it, but it will collide with things. Call
 	 * before addShipsTo(entities) or it won't be rendered.
 	 * 
 	 * @param aiShip
-	 *            The AIShip to be added
-	 */
+	 *        The AIShip to be added */
 	public void addAI(AIShip aiShip) {
 		remotes.forEach(r -> r.addOtherShip(aiShip));
 		player.addOtherShip(aiShip);
@@ -135,10 +130,12 @@ public class MultiplayerShipManager implements ServerShipProvider {
 		aiShip.addOtherShip(player);
 	}
 
+	/** Add a new position packet received from the server. This will update all remote ships.
+	 * 
+	 * @param packet Packet from message of type FULLPOSITIONUPDATE */
 	public void addPacket(byte[] packet) {
 		int j = 0;
-		if (packet.length % SHIP_PACKET_LENGTH != 0)
-			throw new IllegalArgumentException();
+		if (packet.length % SHIP_PACKET_LENGTH != 0) throw new IllegalArgumentException();
 		while (j < packet.length) {
 			byte[] oneShip = new byte[SHIP_PACKET_LENGTH];
 			for (int i = 0; i < SHIP_PACKET_LENGTH; i++) {
@@ -150,6 +147,7 @@ public class MultiplayerShipManager implements ServerShipProvider {
 		// packets.put(packet[0], packet);
 	}
 
+	/** @return The position packet for the player, to be sent to the server as a message of type SENDPLAYERDATA */
 	public byte[] getShipPacket() {
 		return player.export();
 	}
@@ -168,8 +166,7 @@ public class MultiplayerShipManager implements ServerShipProvider {
 	}
 
 	private static TexturedModel makeModel(String model, String texture, Loader loader) {
-		TexturedModel model1 = new TexturedModel(OBJFileLoader.loadOBJ(model, loader),
-				new ModelTexture(loader.loadTexture(texture)));
+		TexturedModel model1 = new TexturedModel(OBJFileLoader.loadOBJ(model, loader), new ModelTexture(loader.loadTexture(texture)));
 		model1.getTexture().setReflectivity(4f);
 		model1.getTexture().setShineDamper(10f);
 		return model1;
